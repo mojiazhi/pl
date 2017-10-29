2238d47d6d8fd370c5c2ce35247eb53e534678db
george.mo <george.mo@kilimall.com>
2017-10-29 22:26:50 +0800

    新增文件

---
+++ web/src/main/java/com/pl/tool/web/common/LogoutController.java
@@ -0,0 +1,18 @@
+/**
+ * kilimall.com Inc.
+ * Copyright (c) 2015-2017 All Rights Reserved.
+ */
+package com.pl.tool.web.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author george.mo
+ * @version $Id: LogoutController.java, v 0.1 2017/10/29 下午5:53 george.mo Exp $
+ */
+@Controller
+@RequestMapping("/logout.html")
+public class LogoutController {
+
+}
